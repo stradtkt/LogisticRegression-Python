0212af6a58336f15c98ea8ae9d0a5960597c84b2
Kevin <stradtkt22@gmail.com>
2019-05-23 10:02:43 -0400

    Added some items to logistic regression

--- lr.py
+++ lr.py
@@ -11,4 +11,36 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 train = pd.read_csv('titanic_train.csv')
-sns.heatmap(train.isnull(), yticklabels=False, char=False, cmap='virdis')
+sns.heatmap(train.isnull(), yticklabels=False, cbar=False, cmap='viridis')
+
+sns.set_style('whitegrid')
+sns.countplot(x='Survived', data=train)
+
+sns.countplot(x='Survived', hue='Sex', data=train)
+sns.countplot(x='Survived', hue='Pclass', data=train)
+
+sns.distplot(train['Age'].dropna(), kde=False, bins=30)
+
+train['Age'].plot.hist(bins=35)
+
+train.info()
+
+sns.countplot(x='SibSp', data=train)
+
+train['Fare'].hist(bins=30, figsize=(10,4))
+
+plt.figure(figsize=(10,7))
+sns.boxplot(x='Pclass', y='Age', data=train)
+
+def impute_age(cols):
+    Age = cols[0]
+    Pclass = cols[1]
+    if pd.isnull(Age):
+        if Pclass == 1:
+            return 37
+        elif Pclass == 2:
+            return 29
+        else:
+            return 24
+    else:
+        return Age
